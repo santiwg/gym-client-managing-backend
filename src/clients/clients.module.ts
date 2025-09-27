4506da7927dae87dc4a5eed1f83fc6fa05b33ee8
ImanolBarrionuevo <barrionuevoimanol@gmail.com>
2025-09-27 12:15:21 -0300

    Importo controlador de ClientGoal en modulo Client

--- src/clients/clients.module.ts
+++ src/clients/clients.module.ts
@@ -8,13 +8,14 @@ import { FeeCollectionService } from './fee-collection/fee-collection.service';
 import { AttendanceService } from './attendance/attendance.service';
 import { ClientGoal } from './client-goal/client-goal.entity';
 import { ClientGoalService } from './client-goal/client-goal.service';
+import { ClientGoalController } from './client-goal/client-goal.controller';
 import { SharedModule } from 'src/shared/shared.module';
 import { Membership } from 'src/membership/membership/membership.entity';
 import { MembershipModule } from 'src/membership/membership.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature(clients_module_entities), SharedModule, MembershipModule],
-  controllers: [ClientController],
+  controllers: [ClientController, ClientGoalController],
   providers: [ClientService, SubscriptionService, FeeCollectionService, AttendanceService, ClientGoalService]
 })
-export class ClientsModule {}
+export class ClientsModule { }
